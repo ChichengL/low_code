13b65d0a4cfe82d77a37fe3dfaa1dee02fd0fd63
ChichengL <2560720865@qq.comm>
2024-03-04 17:03:08 +0800

    初始化

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    jsxBracketSameLine: false, // 修复了这里从 'ksxBracketSameLine' 到 'jsxBracketSameLine'
+    printWidth: 100,
+    proseWrap: 'preserve',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'es5',
+    parser: 'typescript'
+}
